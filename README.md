d5a6066db7cd5796b0a1a465686d050b6657c36d
LuucasGregory <lucasupidiomas@gmail.com>
2025-03-17 22:12:44 -0300

    Inicio do projeto

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Desafio-de-programa-o-da-Marin
-Sistema de Logística com Banco de Dados Este projeto é um sistema de logística desenvolvido com C# e ASP.NET Core, utilizando SQL Server como banco de dados. O objetivo é criar uma solução eficiente para gerenciamento de estoque, rastreamento de pedidos e otimização de entregas. uaiugasdugausidausg
+# Desafio-Marin
---
+++ src/Servidor/Desafio.Marin.API/Controllers/ArquivoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio.Marin.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArquivoController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult ProcessarArquivo(IFormFile file)
+        {
+            if (Path.GetExtension(file.FileName) != "cnab")
+            {
+                return BadRequest(new { Erro = "Arquivo no formato incorreto" });
+            }
+
+            return Ok();
+        }
+    }
+}
