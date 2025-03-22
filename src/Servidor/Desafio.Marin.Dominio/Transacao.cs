c0034a1e778f80f7ed26ab300bbaf05200f59307
LuucasGregory <lucasupidiomas@gmail.com>
2025-03-22 17:23:34 -0300

    Renomeado classe de dominio para Transacao

--- src/Servidor/Desafio.Marin.Dominio/Transacao.cs
+++ src/Servidor/Desafio.Marin.Dominio/Transacao.cs
@@ -1,21 +1,22 @@
 ﻿namespace Desafio.Marin.Dominio
 {
-    public class ArquivoCNAB
+    public class Transacao
     {
+        public Guid Id { get; set; }
         public TipoTransacao Tipo { get; set; }
-        public DateTime Data { get; set; }
+        public DateTimeOffset Data { get; set; }
         public decimal Valor { get; set; }
-        public string CPF { get; set; }
+        public string Cpf { get; set; }
         public string Cartao { get; set; }
         public string Representante { get; set; }
         public string NomeLoja { get; set; }
 
-        public ArquivoCNAB(TipoTransacao tipo, DateTime data, decimal valor, string cpf, string cartao, string representante, string nomeLoja)
+        public Transacao(TipoTransacao tipo, DateTimeOffset data, decimal valor, string cpf, string cartao, string representante, string nomeLoja)
         {
             Tipo = tipo;
             Data = data;
             Valor = valor;
-            CPF = cpf;
+            Cpf = cpf;
             Cartao = cartao;
             Representante = representante;
             NomeLoja = nomeLoja;
